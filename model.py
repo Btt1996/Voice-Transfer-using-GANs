6d833d7b8d2dfd9640a20599c1f5858582d963cb
ahmed taboubi <61942133+Btt1996@users.noreply.github.com>
2023-01-18 22:09:45 +0100

    Create model.py

---
+++ model.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# Define the generator
+def make_generator_model():
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
+
+    model.add(keras.layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256)
+
+    model.add(keras.layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
+
+    model.add(keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
+
+    model.add(keras.layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    return model
+
+# Define the discriminator
+def make_discriminator_model():
+    model = keras.Sequential()
+    model.add(keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=[28, 28, 1]))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.Dropout(0.3))
+
+    model.add(keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.Dropout(0.3))
+
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(1))
+
+    return model
